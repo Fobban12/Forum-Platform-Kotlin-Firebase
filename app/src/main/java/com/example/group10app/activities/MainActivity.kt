95e7ec420afe87351391dba682a033638ef064d0
Jere Muikku <Muikku.jere@hotmail.com>
2023-03-15 15:29:53 +0200

    Added Composable and other dependencies

--- app/src/main/java/com/example/group10app/activities/MainActivity.kt
+++ app/src/main/java/com/example/group10app/activities/MainActivity.kt
@@ -3,6 +3,17 @@ package com.example.group10app.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.group10app.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,3 +21,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
