7a7b653921cdb0831890a5e05e24be0c68ba0403
thongdang2708 <duythong2708@gmail.com>
2023-03-27 12:55:49 +0300

    Complete like functionalities for forum

--- app/src/main/java/com/example/kotlin_application/screens/SingleForumScreen.kt
+++ app/src/main/java/com/example/kotlin_application/screens/SingleForumScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -30,6 +31,7 @@ import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -42,15 +44,19 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import com.example.kotlin_application.data.Forum
+import com.example.kotlin_application.data.Like
+import com.example.kotlin_application.data.LikeInput
 import com.example.kotlin_application.navigation.Screens
+import com.example.kotlin_application.ui.theme.goldYellowHex
 import com.example.kotlin_application.viewmodel.CommentViewModel
 import com.example.kotlin_application.viewmodel.ForumViewModel
+import com.example.kotlin_application.viewmodel.LikeViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.QuerySnapshot
 
 @ExperimentalComposeUiApi
 @Composable
-fun SingleForumScreen (navController: NavController, forumId: String, viewModel: ForumViewModel = androidx.lifecycle.viewmodel.compose.viewModel(), commentViewModel: CommentViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+fun SingleForumScreen (navController: NavController, forumId: String, viewModel: ForumViewModel = androidx.lifecycle.viewmodel.compose.viewModel(), commentViewModel: CommentViewModel = androidx.lifecycle.viewmodel.compose.viewModel(), likeViewModel: LikeViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
 
     //Test with single forum screen based on dummy data
 
@@ -63,12 +69,15 @@ fun SingleForumScreen (navController: NavController, forumId: String, viewModel:
         viewModel.getSingleForum(forumId);
     }
 
+    //Set effect to fetch likes
+    LaunchedEffect(forumId, likeViewModel ) {
+        likeViewModel.fetchLikes(forumId);
+    }
+
 
     //Single forum data
     val singleForum = viewModel.singleForum;
-
-
-
+    
     //Check user is logged in or not
     val checkUserIsNull = remember(FirebaseAuth.getInstance().currentUser?.email) {
         FirebaseAuth.getInstance().currentUser?.email.isNullOrEmpty()
@@ -76,6 +85,23 @@ fun SingleForumScreen (navController: NavController, forumId: String, viewModel:
 
     //Get uid from firebase
     val uid = FirebaseAuth.getInstance()?.uid;
+
+    //Get username from firebase
+    val username = FirebaseAuth.getInstance()?.currentUser?.email?.split("@")?.get(0);
+    
+    //Get like from like view model
+    val likes = likeViewModel.likes;
+    
+    //Check like or dislike by user
+    val likeOrDislike = likes.any { it?.userId == uid && it?.forumId == forumId }
+
+    //Get context for Toast
+    val context = LocalContext.current;
+
+    //Get like by forum id and userid
+
+
+
 
     
     Scaffold(
@@ -141,6 +167,48 @@ fun SingleForumScreen (navController: NavController, forumId: String, viewModel:
                     }
                 }
                 
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Row(horizontalArrangement = Arrangement.Center) {
+                    if (likes.size == 0) {
+                        Text(text = "0 like for this forum", style = TextStyle(color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 16.sp), modifier = Modifier.padding(12.dp))
+                    } else if (likes?.size == 1 && !likeOrDislike) {
+                        Text(text = "${likes.size} person likes for this forum", style = TextStyle(color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 13.sp), modifier = Modifier.padding(12.dp))
+                    } else if (likes?.size == 1 && likeOrDislike) {
+                        Text(text = "You like for this forum", style = TextStyle(color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 13.sp), modifier = Modifier.padding(12.dp))
+                    } else {
+                        Text(text = if (!likeOrDislike) "${likes.size} people like this forum" else "You and other ${likes.size -1} people like this forum", style = TextStyle(color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 13.sp), modifier = Modifier.padding(12.dp))
+                    }
+
+                    Spacer(modifier = Modifier.width(10.dp))
+                    if (!likeOrDislike && !checkUserIsNull) {
+                    Button(onClick = { likeViewModel.saveLike(LikeInput(forumId = forumId, userId = uid, username = username), context = context) }, colors = ButtonDefaults.buttonColors(
+                        backgroundColor = MaterialTheme.colors.onBackground,
+                        contentColor = Color.White)) {
+                        Row(horizontalArrangement = Arrangement.Center) {
+                            Icon(imageVector = Icons.Default.Favorite, contentDescription = "Like")
+                            Spacer(modifier = Modifier.width(5.dp))
+                            Text(text = "Like", modifier = Modifier.padding(2.dp))
+                        }
+                    }
+                } else if (likeOrDislike && !checkUserIsNull) {
+                    val singleLike = likes.filter { it?.forumId == forumId && it?.userId == uid }?.get(0);
+                        
+                        
+
+                     Button(onClick = { likeViewModel.deleteLike(singleLike?.id as String, context = context) }, colors = ButtonDefaults.buttonColors(
+                        backgroundColor = MaterialTheme.colors.onBackground,
+                        contentColor = Color.White)) {
+                        Row() {
+                            Icon(imageVector = Icons.Default.Favorite, contentDescription = "Like", tint = goldYellowHex)
+                            Spacer(modifier = Modifier.width(5.dp))
+                            Text(text = "Like", modifier = Modifier.padding(2.dp), color = goldYellowHex)
+                        }
+                    }
+                }
+                }
+
+
                 Spacer(modifier = Modifier.height(5.dp))
                 Text(text = "Comments: ", style = TextStyle(color = MaterialTheme.colors.onBackground, fontWeight = FontWeight.Bold, fontSize = 18.sp), modifier = Modifier.padding(3.dp))
                 Spacer(modifier = Modifier.height(5.dp))
