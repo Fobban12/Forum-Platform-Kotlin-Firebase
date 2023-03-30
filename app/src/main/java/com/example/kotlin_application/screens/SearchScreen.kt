06753afc765a54157aae5a1ba3cea20bb3f18182
Bao <tranxbao@gmail.com>
2023-03-30 04:37:53 +0300

    deleted some unnecessary stuff in Search Screen

--- app/src/main/java/com/example/kotlin_application/screens/SearchScreen.kt
+++ app/src/main/java/com/example/kotlin_application/screens/SearchScreen.kt
@@ -9,20 +9,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
+
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.focus.FocusState
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -32,8 +25,7 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
-import com.example.kotlin_application.data.Forum
-import com.example.kotlin_application.data.ProductList
+
 import com.example.kotlin_application.navigation.Screens
 import com.example.kotlin_application.viewmodel.ForumViewModel
 import com.google.firebase.auth.FirebaseAuth
@@ -75,36 +67,16 @@ fun SearchScreen(navController: NavController ,viewModel: ForumViewModel = andro
             TopAppBar(
                 navigationIcon = {
 
-                    //Thong: the thing I test, I keep Bao's things in comment as below
                     IconButton(onClick = {navController.popBackStack()}) {
                         Icon(
                             imageVector = Icons.Default.ArrowBack,
                             contentDescription = "Go Back"
                         )
                     }
-//                    SearchTopBar(
-//                        navController,
-//                        IconClick = {
-//                            navController.navigate(Screens.MainScreen.name)
-//                        })
 
                 },
                 title = {
-                    //Thong: the stuff I test, I keep Bao's things in the comment below
-//                    Row(
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                    ) {
 
-//                        SearchBar(
-//                            hint = " ",
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .padding(start = 0.dp, end = 45.dp)
-//
-//                        ){
-//
-//                        }
 
 
                         OutlinedTextField(
@@ -193,85 +165,4 @@ fun SearchScreen(navController: NavController ,viewModel: ForumViewModel = andro
 
 
 
-
-@ExperimentalComposeUiApi
-@Composable
-fun SearchTopBar(navController: NavController, IconClick: () -> Unit) {
-    val scaffoldState = rememberScaffoldState();
-
-    TopAppBar(
-        navigationIcon = {
-            IconButton(onClick = IconClick) {
-                Icon(
-                    imageVector = Icons.Default.ArrowBack,
-                    contentDescription = "Go Back"
-                )
-            }
-        },
-        title = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-            ) {
-
-              SearchBar(
-                   hint = " ",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 0.dp, end = 45.dp)
-
-                ){
-
-              }
-            }
-        },
-        backgroundColor = MaterialTheme.colors.onBackground,
-        contentColor = MaterialTheme.colors.onSecondary
-    )
-
-
-}
-
-
-@Composable
-fun SearchBar(
-    modifier: Modifier = Modifier,
-    hint: String = "",
-    onSearch: (String) -> Unit = {},
-) {
-    var text = remember {
-        mutableStateOf("")
-    }
-
-    var isHintDisplayed by remember {
-        mutableStateOf(hint != "")
-    }
-
-    Box(modifier = modifier) {
-        OutlinedTextField(
-            value = text.value,
-            onValueChange = {
-                text.value = it
-                onSearch(it)
-            },
-            maxLines = 1,
-            singleLine = true,
-            textStyle = TextStyle(color = Color.Black),
-            modifier = Modifier
-                .fillMaxWidth()
-                .shadow(5.dp, CircleShape)
-                .background(Color.White, CircleShape)
-                .padding(horizontal = 20.dp, vertical = 12.dp)
-
-                    )
-                    if (isHintDisplayed) {
-                        Text(
-                            text = hint,
-                            color = Color.LightGray,
-                            modifier = Modifier
-                                .padding(horizontal = 20.dp, vertical = 12.dp)
-                        )
-                    }
-                }
-    }
 
