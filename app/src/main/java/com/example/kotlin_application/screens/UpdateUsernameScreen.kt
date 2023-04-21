f432c81bf6ee3ad90a87f4ff64df502887df9e6d
Jere Muikku <Muikku.jere@hotmail.com>
2023-04-21 23:22:26 +0300

    Minor text changes

--- app/src/main/java/com/example/kotlin_application/screens/UpdateUsernameScreen.kt
+++ app/src/main/java/com/example/kotlin_application/screens/UpdateUsernameScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.kotlin_application.navigation.Screens
 import com.example.kotlin_application.viewmodel.UserProfileViewModel
 
 @Composable
@@ -105,7 +106,7 @@ fun UpdateUsernameScreen (navController: NavController, userProfileId : String) 
                         Toast.makeText(context, "New username is invalid and it must include at least 5 characters", Toast.LENGTH_LONG).show()
                     } else {
                         userProfileViewModel.updateUsername(user_profile?.id as String, newUsername.value as String, context = context, user_profile?.userId as String );
-                        navController.popBackStack();
+                        navController.navigate(Screens.ProfileScreen.name)
                         Log.d("Successfully", "Successfully!")
                     }
                     keyboardController.clearFocus()
